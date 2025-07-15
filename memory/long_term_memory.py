9c9061ef715aabf7bbc478a3e58c7376e28acc39
eddlgtm <edward@robopsychology.io>
2025-07-15 13:55:19 +0200

    Add github action

--- memory/long_term_memory.py
+++ memory/long_term_memory.py
@@ -9,6 +9,7 @@ class LongTermMemory:
     """
     A class for long term memory
     """
+
     def __init__(self, file_path: Path):
         self.file_path = file_path
         if file_path.is_file():
@@ -17,9 +18,20 @@ class LongTermMemory:
         else:
             self.memory = {}
 
+        self.summary = ""
+
     def add_short_term_memory(self, memory: ShortTermMemory):
+        # Add short term memory to long term memory
+        # time stamps as keys organise when they're added
         self.memory.append({time.gmtime(): memory})
 
     def save(self):
+        # We can store memory as a json file for now
         with open(file=self.file_path, mode="w") as f:
             f.writelines(json.dumps(self.memory))
+
+    def summarise(self):
+        # We need to take all the memories and condense them into a summary
+        # that can be fed to an LLM in its context, maybe with some sort of
+        # prioritisation for recent memories
+        raise NotImplementedError
